0ed8683dee9e132c2b1a85a8cd5d02daa36e5df3
Daniel <D4n13l3k00@yandex.ru>
2025-11-28 21:30:04 +0300

    fix: compatibility with new API chore: remove banner from stream launch menu feat: embed welcome image in build using base64

--- src/NitroStreams.jsx
+++ src/NitroStreams.jsx
@@ -1,19 +1,20 @@
 /**!
  * @name NitroStreams
- * @version 1.0.0
+ * @version 1.2.0
  * @description Acticvation of Nitro Streams
  * @author D4n13l3k00
  *
  * @website https://t.me/D4n13l3k00
  */
 
+import info from "./assets/info.png?base64";
+
 module.exports = class NitroStreams {
-  uiInjectInterval = null;
   nitroInterval = null;
-  getCurrentUser = BdApi.findModuleByProps("getCurrentUser").getCurrentUser;
+  getCurrentUser = BdApi.Webpack.getStore("UserStore").getCurrentUser;
 
   showInfo() {
-    BdApi.alert(
+    BdApi.UI.alert(
       "NitroStreams",
       <div
         style={{
@@ -32,7 +33,7 @@ module.exports = class NitroStreams {
         }}
       >
         <img
-          src="https://discord.com/assets/ff5b4f3466b1e217ba845ce0540da563.png"
+          src={"data:image/png;base64," + info}
           alt=""
           style={{ width: "100%" }}
         />
@@ -49,38 +50,9 @@ module.exports = class NitroStreams {
   }
 
   load() {
-    if (BdApi.loadData("NitroStreams", "loaded") !== true) {
+    if (BdApi.Data.load("NitroStreams", "loaded") !== true) {
       this.showInfo();
-      BdApi.saveData("NitroStreams", "loaded", true);
-    }
-  }
-
-  caption() {
-    let qualitySettingsContainer = document.querySelector(
-      "div[class^='qualitySettingsContainer']"
-    );
-    let caption = document.getElementsByClassName("NitroStreams-caption");
-    if (qualitySettingsContainer !== null && caption.length === 0) {
-      const _h6 = (
-        <h6
-          style={{
-            marginTop: "10px",
-            marginBottom: "5px",
-            fontSize: "14px",
-            fontWeight: "500",
-            color: "var(--interactive-normal)",
-            lineHeight: "16px",
-            textTransform: "uppercase",
-            textAlign: "center",
-          }}
-          className="NitroStreams-caption"
-        >
-          Made with ❤ by D4n13l3k00
-        </h6>
-      );
-      const output = document.createElement("div");
-      BdApi.ReactDOM.render(_h6, output);
-      qualitySettingsContainer.appendChild(output);
+      BdApi.Data.save("NitroStreams", "loaded", true);
     }
   }
 
@@ -90,13 +62,11 @@ module.exports = class NitroStreams {
 
   start() {
     this.old_account_type = this.getCurrentUser().premiumType;
-    this.uiInjectInterval = setInterval(this.caption, 1000);
     this.setNitro();
     this.nitroInterval = setInterval(this.setNitro, 5000);
   }
 
   stop() {
-    clearInterval(this.uiInjectInterval);
     clearInterval(this.nitroInterval);
     this.getCurrentUser().premiumType = this.old_account_type;
   }
--- vite.config.js
+++ vite.config.js
@@ -41,9 +41,22 @@ const meta = () => {
   return meta + " */\n";
 };
 
+const base64Loader = {
+  name: "base64-loader",
+  transform(_, id) {
+    const [path, query] = id.split("?");
+    if (query != "base64") return null;
+
+    const data = fs.readFileSync(path);
+    const base64 = data.toString("base64");
+
+    return `export default '${base64}';`;
+  },
+};
+
 export default defineConfig(({ command, mode }) => {
 
-  let plugins = [viteSingleFile()];
+  let plugins = [base64Loader, viteSingleFile()];
 
   if (mode === "development") {
     plugins.push(copyToPlugins());
